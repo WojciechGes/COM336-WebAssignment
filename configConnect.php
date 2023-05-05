8fd5add2946d1f7f900e2da09213da1efcea8f73
AdrianGorm <95683429+AdrianGorm@users.noreply.github.com>
2023-05-05 19:53:49 +0100

    UPUP
    UPUP

--- configConnect.php
+++ configConnect.php
@@ -3,7 +3,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 0);
 $servername='com336aws.cdybmgd0rq3p.us-east-1.rds.amazonaws.com';
 $username='admin_com336';
-$password='iwilldeletethisafter20';
+$password='Iwilldeletethisafter20';
 $dbname = "forddealership";
 $mySqlConn = mysqli_connect($servername,$username,$password,"$dbname");
 if(!$mySqlConn){
