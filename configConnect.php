964180342e228dd35d65f0727cdf75b71f205665
AdrianGorm <95683429+AdrianGorm@users.noreply.github.com>
2023-05-05 17:09:07 +0100

    Update configConnect.php
    AWS ConfigConnect.php file update to use correct Servername, Username and Password

--- configConnect.php
+++ configConnect.php
@@ -1,9 +1,9 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 0);
-$servername='localhost';
-$username='root';
-$password='';
+$servername='com336aws.cdybmgd0rq3p.us-east-1.rds.amazonaws.com';
+$username='admin_com336';
+$password='iwilldeletethisafter20';
 $dbname = "forddealership";
 $mySqlConn = mysqli_connect($servername,$username,$password,"$dbname");
 if(!$mySqlConn){
